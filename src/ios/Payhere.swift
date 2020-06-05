56fccfc0f06bc9f42f855b2d261d93cb24cbe0f8
Nipuna Harshana <nipuna.harshana@infor.com>
2020-06-05 19:34:09 +0530

    IOS - Checkout method implemented

--- src/ios/Payhere.swift
+++ src/ios/Payhere.swift
@@ -1,18 +1,48 @@
+import payHereSDK
+import UIKit
+
 /*
 * Notes: The @objc shows that this class & function should be exposed to Cordova.
 */
-@objc(Payhere) class Payhere : CDVPlugin {
+@objc(Payhere) class Payhere : CDVPlugin,PHViewControllerDelegate {
+
+    var mCalendarAccountsCallbackContext:String?
+    let merchandID = "<<>>"
+    var initRequest : PHInitialRequest?
+    
+    func onResponseReceived(response: PHResponse<Any>?) {
+        let pluginResult:CDVPluginResult;
+        if(response?.isSuccess())!{
+            guard let resp = response?.getData() as? StatusResponse else{ return }
+                   
+            //Payment Sucess
+            pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: resp.toJSONString());
+                   
+        }else{
+            let msg = response?.getMessage()
+            pluginResult = CDVPluginResult (status: CDVCommandStatus_ERROR, messageAs: msg);   
+        }
+        
+        // Send the function result back to Cordova.
+        self.commandDelegate!.send(pluginResult, callbackId: self.mCalendarAccountsCallbackContext);
+    }
+    
+    func onErrorReceived(error: Error) {
+        print("✋ Error",error)
+        let pluginResult = CDVPluginResult (status: CDVCommandStatus_ERROR, messageAs: error.localizedDescription);
+        self.commandDelegate!.send(pluginResult, callbackId: self.mCalendarAccountsCallbackContext);
+    }
+    
   @objc(checkout:) // Declare your function name.
   func checkout(command: CDVInvokedUrlCommand) { // write the function code.
-    /* 
-     * Always assume that the plugin will fail.
-     * Even if in this example, it can't.
-     */
-    // Set the plugin result to fail.
-    var pluginResult = CDVPluginResult (status: CDVCommandStatus_ERROR, messageAs: "The Plugin Failed");
-    // Set the plugin result to succeed.
-        pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "The plugin succeeded");
-    // Send the function result back to Cordova.
-    self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
+    self.mCalendarAccountsCallbackContext = command.callbackId
+    
+    let item1 = Item(id: "001", name: "PayHere Test Item 01", quantity: 1, amount: 25.00)
+    let item2 = Item(id: "002", name: "PayHere Test Item 02", quantity: 2, amount: 25.0)
+
+    self.initRequest = PHInitialRequest(merchantID: self.merchandID, notifyURL: "", firstName: "Pay", lastName: "Here", email: "test@test.com", phone: "+9477123456", address: "Colombo", city: "Colombo", country: "Sri Lanka", orderID: "001", itemsDescription: "PayHere SDK Sample", itemsMap: [item1,item2], currency: .LKR, amount: 50.00, deliveryAddress: "", deliveryCity: "", deliveryCountry: "", custom1: "custom 01", custom2: "custom 02")
+    
+    PHPrecentController.precent(from: self.viewController, isSandBoxEnabled: false, withInitRequest: self.initRequest!, delegate: self)
+
   }
 }
